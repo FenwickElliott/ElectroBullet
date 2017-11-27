ae42ef92f8184a600d5103c05c76cbf8f1f4f703
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2017-11-27 12:28:01 -0500

    get taking optional parameters

--- util.js
+++ util.js
@@ -12,10 +12,10 @@ class Wait {
     };
 };
 
-function get(path) {
+function get(path, hostname, encoding) {
     return new Promise( (resolve, reject) => {
         let options = {
-            hostname: 'api.pushbullet.com',
+            hostname: hostname || 'api.pushbullet.com',
             path: path,
             headers: {
                 "Access-Token": keys.token,
@@ -25,6 +25,7 @@ function get(path) {
         let temp = '';
         req = https.request(options, (res) => {
             if ( res.statusCode != 200 ) { reject( res.statusCode + " on " + path ) };
+            if (encoding) { res.setEncoding(encoding) };
             res.on('data', (d) => { temp += d });
             res.on('end', () => { resolve(temp) });
             res.on('error', (e) => { reject(e) });
